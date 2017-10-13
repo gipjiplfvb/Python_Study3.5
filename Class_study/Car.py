75792162ddd535e0b7fccc91b20b35822f28c7b6
gipjiplfvb <ma_hao@foxmail.com>
2017-10-13 12:26:31 +0800

    添加一点注释而已

--- Class_study/Car.py
+++ Class_study/Car.py
@@ -47,12 +47,12 @@ class Car():
 		"""将里程表读数增加指定的量"""
 		self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2016)
-print (my_new_car.get_descriptive_name())
+#my_new_car = Car('audi', 'a4', 2016)
+#print (my_new_car.get_descriptive_name())
 #my_new_car.read_odometer()
-my_new_car.odometer_reading = 23
+#my_new_car.odometer_reading = 23
 #my_new_car.read_odometer()
-my_new_car.update_odometer(10)
-my_new_car.read_odometer()
-my_new_car.increment_odometer(90)
-my_new_car.read_odometer()
+#my_new_car.update_odometer(10)
+#my_new_car.read_odometer()
+#my_new_car.increment_odometer(90)
+#my_new_car.read_odometer()
