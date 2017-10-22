814d963ddb7fc795ef8efb1d0538ff9b8c51f5be
gipjiplfvb <ma_hao@foxmail.com>
2017-10-22 14:40:12 +0800

    2017.10.22 测试成功。哈哈

--- test/银行存钱取钱(最简）.py
+++ test/银行存钱取钱(最简）.py
@@ -1,6 +1,19 @@
+def atm_1():
+	"""帮助信息"""
+	menu_2 = """选择下面提供的业务，：
+	////////////////////////////////
+	|| 余额请按"2"
+	|| 存款请按"3"
+	|| 取款请按"4"		
+	|| 退出请按“5” 
+	///////////////////////////////
+	"""
+	print(menu_2)
+
 def atm_2():
 	"""显示总金额"""
 	print(money)
+
 def atm_3():
 	"""存钱"""
 	global money
@@ -8,7 +21,6 @@ def atm_3():
 	money += money_add
 	print("您现在的总金额是%s" % (money))
 	return money
-
 
 def atm_4():
 	"""取钱"""
@@ -18,35 +30,89 @@ def atm_4():
 	print("您现在的总金额是%s" % (money))
 	return money
 
-
 def atm_5():
 	"""退出"""
 	print("感谢你使用屌丝银行！")
+def atm_6():
+	"""错误提示"""
+	press_1 = input("你的选择有误，请重新输入，反回上一层就按1，退出请按5")
+	return press_1
 money = 999
-while True:
-	menu = """请选择你要的业务：
-		***********************************
-		余额请按“2”
-		存款请按“3”
-		取款请按“4”
-		返回主菜单请按“1”
-		退出请按“5”
-		***********************************
-		"""
-	press = input(menu)
-	if press == '2':
+loop = True
+menu_1 = """请选择你要的业务：
+==============================
+|| 余额请按"2"
+|| 存款请按"3"
+|| 取款请按"4"		
+|| 退出请按“5” 
+==============================
+请输入：
+"""
+while loop:
+	press = input(menu_1)
+	if press == "1":
+		atm_1()
+	elif press == '2':
 		atm_2()
+		while loop:
+			chosse_1 = input("""
+			************
+			*返回请按1：*			
+			*退出请按5：*
+			************
+			""")
+			if chosse_1 == '1':
+				break
+			elif chosse_1 == '5':
+				atm_5()
+				loop = False
 	elif press == '3':
 		atm_3()
+		while loop:
+			chosse_1 = input("""
+			************
+			*返回请按1：*
+			*继续请按3：*
+			*退出请按5：*
+			************
+			""")
+			if chosse_1 == '1':
+				break
+			elif chosse_1 == '3':
+				atm_3()
+			elif chosse_1 == '5':
+				atm_5()
+				loop = False
+			else:
+				atm_1()
 	elif press == '4':
 		atm_4()
+		while loop:
+			chosse_1 = input("""
+			************
+			*返回请按1：*
+			*继续请按4：*
+			*退出请按5：*
+			************
+			""")
+			if chosse_1 == '1':
+				break
+			elif chosse_1 == '4':
+				atm_3()
+			elif chosse_1 == '5':
+				atm_5()
+				loop = False
 	elif press == '5':
 		atm_5()
 		break
 	else:
-		print(menu)
+		if atm_6() == '1':
+			atm_1()
+		else:
+			break
 
-print ("您现在的存款是%d"%money)
+print("感谢你使用屌丝银行！")
+print("您现在的存款是%d"%money)
 
 '''menu = """请选择你要的业务：
 	***********************************
@@ -58,7 +124,8 @@ print ("您现在的存款是%d"%money)
 	***********************************
 	"""
 press = input(menu)
-money = 999
+money = 9992
+
 #new_money = 0
 
 class Atm():
